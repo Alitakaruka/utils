8a7898c53a9d7332af8e7ecaa6b9a38c6baef885
Ваше Имя <you@example.com>
2026-03-29 04:12:53 +0300

    avr

--- AvrInstall.sh
+++ AvrInstall.sh
@@ -1,5 +1,42 @@
 sudo apt update
 sudo apt install gcc-avr avr-libc binutils-avr avrdude
-sudo usermod -aG dialout $USER
+sudo apt install make
 
-echo "All done"
+
+
+#rules 
+#com port
+sudo usermod -aG dialout $USER
+#usbasp
+echo "Setting up USBasp udev rules..."
+
+RULE_FILE="/etc/udev/rules.d/usbasp.rules"
+
+# Проверка root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root (sudo)"
+  exit 1
+fi
+
+# Создаём правило
+echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="16c0", ATTR{idProduct}=="05dc", MODE="0666"' > $RULE_FILE
+
+echo "Rule written to $RULE_FILE"
+
+# Перезагрузка правил
+udevadm control --reload-rules
+udevadm trigger
+
+echo "Udev rules reloaded"
+
+echo "Now unplug and replug your USBasp device"
+
+# Проверка
+echo "Checking device list:"
+lsusb | grep 16c0:05dc
+
+echo "Done."
+
+
+
+echo "All done. Please reboot the computer."
